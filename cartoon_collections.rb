c73cdc55e530bc39c3a13e588ed385f7fae35c66
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-04 16:12:50 +1000

    done with lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarf_arr
+  dwarf_arr.each.with_index { |dwarf, i| puts "#{i + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls 
+  new_calls = planeteer_calls.map do |item| 
+    item.capitalize + "!"
+  end
+  puts new_calls
+  new_calls
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls planeteer_calls
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese word_arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_match = cheese_types.find do |cheese|
+    word_arr.include?(cheese)
+  end
+  cheese_match
 end
+
